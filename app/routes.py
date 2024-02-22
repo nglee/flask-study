f01ed54cf21620fe94c79d6ee47ce43716428549
Namgoo Lee <devnglee@gmail.com>
2024-02-22 21:26:16 +0900

    chapter 2

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello, World!"
+    user = { "username": "Namgoo" }
+    posts = [
+        {
+            "author": { "userame": "Namgoo" },
+            "body": "Beautiful day in Portland!"
+        },
+        {
+            "author": { "userame": "Namgoo" },
+            "body": "Beautiful day in Portland!"
+        },
+    ]
+    return render_template("index.html", user=user, posts=posts)
